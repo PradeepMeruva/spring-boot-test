1bd148fc74e9d10609ca05812eb7033647335ead
Pradeep Meruva <pradeep.meruva@gmail.com>
2020-12-06 18:09:50 -0600

    code farmated

--- src/test/java/com/pradeep/springboot/test/controller/CustomerControllerTest.java
+++ src/test/java/com/pradeep/springboot/test/controller/CustomerControllerTest.java
@@ -15,31 +15,27 @@ import org.springframework.test.web.servlet.MockMvc;
 import com.pradeep.springboot.test.domain.Customer;
 import com.pradeep.springboot.test.service.CustomerService;
 
-
 @WebMvcTest
 @Import(CustomerService.class)
 public class CustomerControllerTest {
-	
+
 	@MockBean
 	CustomerService customerService;
-	
+
 	@Autowired
 	private MockMvc mockMvc;
-	
-	
+
 	@Test
 	public void shouldReturnDefaultMessage() throws Exception {
-		
-		Customer customer =  new Customer();
+
+		Customer customer = new Customer();
 		customer.setId("123");
 		customer.setFirstName("John");
-		customer.setLastName("Miller");		
-		
-		Mockito.when(customerService.getCustomer("123")).thenReturn(customer);		
+		customer.setLastName("Miller");
+
+		Mockito.when(customerService.getCustomer("123")).thenReturn(customer);
 		this.mockMvc.perform(get("/customers/123")).andDo(print()).andExpect(status().isOk());
-				
+
 	}
-	
-	
-	
+
 }
--- src/test/java/com/pradeep/springboot/test/service/CustomerServiceTest.java
+++ src/test/java/com/pradeep/springboot/test/service/CustomerServiceTest.java
@@ -17,55 +17,39 @@ import org.springframework.test.context.junit.jupiter.SpringExtension;
 import com.pradeep.springboot.test.domain.Customer;
 import com.pradeep.springboot.test.repo.CustomerRepo;
 
-
-
-
-
 @ExtendWith(SpringExtension.class)
 @Import(CustomerRepo.class)
 public class CustomerServiceTest {
-	
-	
-	
+
 	@TestConfiguration
-    static class CustomerServiceImplTestContextConfiguration {
- 
-        @Bean
-        public CustomerService customerService() {
-            return new CustomerServiceImpl();
-        }
-    }
-	
-	
-	
-	
+	static class CustomerServiceImplTestContextConfiguration {
+
+		@Bean
+		public CustomerService customerService() {
+			return new CustomerServiceImpl();
+		}
+	}
+
 	@MockBean
 	private CustomerRepo customerRepo;
-	
+
 	@Autowired
 	private CustomerService customerService;
-	
-	
-	
-	
-	
-	
+
 	@Test
 	void getCustomer() {
-		
-		Customer customer =  new Customer();
+
+		Customer customer = new Customer();
 		customer.setId("123");
 		customer.setFirstName("John");
-		customer.setLastName("Miller");	
-		
+		customer.setLastName("Miller");
+
 		Mockito.when(customerRepo.findById("AnyId")).thenReturn(Optional.of(customer));
-		
+
 		Customer custo = customerService.getCustomer("AnyId");
-		
+
 		assertThat(custo.getFullName()).isEqualTo("John Miller");
-		
-		
+
 	}
-	
 
 }
